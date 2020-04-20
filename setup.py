76f05f23d41aa57e4e4b82af4080f9f112f83242
Lion Krischer <lion.krischer@gmail.com>
2020-04-20 14:56:03 +0200

    Proper pypi description.

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
 from setuptools import setup, find_packages
 
+import os
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
 setup(
     name="asdf-sextant",
     version="0.1.0",
     author="Lion Krischer",
     author_email="lion.krischer@gmail.com",
+    description="GUI to explore ASDF files",
+    long_description="",
+    long_description_content_type="text/markdown",
     classifiers=["Programming Language :: Python :: 3"],
     packages=find_packages(),
     package_data={
